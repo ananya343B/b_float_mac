4020a95099c2d06d154c44999339788739826c7f
ananya343B <142582353+ananya343B@users.noreply.github.com>
2024-04-16 19:07:25 +0530

    Update mul_tb.sv

--- mul_tb.sv
+++ mul_tb.sv
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12.04.2024 18:52:41
+// Create Date: 16.04.2024 18:29:31
 // Design Name: 
 // Module Name: mul_tb
 // Project Name: 
@@ -46,46 +46,49 @@ module mul_tb();
   // Test procedure
   initial begin
     // Display the test case header
-    $display("Test Case\t| a \t| b \t| Expected c\t| Observed c");
-    #100;
+    $display("Test Case\t| a \t| b \t| c");
+    #10;
     // Test Case 1
-    a = 16'h3F80; // 1.0 
+    a = 16'h0020; // SUBNORMAL (1.1 * 2^-127)
     b = 16'h3F80; // 1.0 
-    #100; 
-    $display("1\t\t| %h\t| %h\t| ???\t\t| %h", a, b, c);
+    #10; 
+    $display("1\t\t| %h\t| %h |\t| %h", a, b, c);
     
     // Test Case 2
     a = 16'h3F00; // 0.5
     b = 16'h3F80; // 1.0 
-    #100; 
-    $display("2\t\t| %h\t| %h\t| ???\t\t| %h", a, b, c);
+    #10; 
+    $display("2\t\t| %h\t| %h |\t| %h", a, b, c);
     
     // Test Case 3
-    a = 16'h0000; // 0.0 
+    a = 16'h8000; // -0.0 
     b = 16'h3F80; // 1.0
-    #100; 
-    $display("3\t\t| %h\t| %h\t| 0\t\t| %h", a, b, c);
+    #10; 
+    $display("3\t\t| %h\t| %h |\t| %h", a, b, c);
     
     // Test Case 4
     a = 16'h3F80; // 1.0 
     b = 16'h0000; // 0.0 
-    #100; 
-    $display("4\t\t| %h\t| %h\t| 0\t\t| %h", a, b, c);
+    #10; 
+    $display("4\t\t| %h\t| %h |\t| %h", a, b, c);
     
     // Test Case 5
     a = 16'h3FC0; // 1.5 
     b = 16'h4000; // 2.0 
-    #100; 
-    $display("5\t\t| %h\t| %h\t| ???\t\t| %h", a, b, c);
+    #10; 
+   $display("5\t\t| %h\t| %h |\t| %h", a, b, c);
     
     // Test Case 6
     a = 16'hC000; // -2.0
     b = 16'h3FC0; // 1.5 
-    #100; 
-    $display("6\t\t| %h\t| %h\t| ???\t\t| %h", a, b, c);
-    
+    #10; 
+    $display("6\t\t| %h\t| %h |\t| %h", a, b, c);
+    //Test Case 7
+    a = 16'h8000; //-0
+    b = 16'h0000; //+0
+    $display("7\t\t| %h\t| %h |\t| %h", a, b, c);
+    #10;
     $finish;
   end
 
 endmodule
-
